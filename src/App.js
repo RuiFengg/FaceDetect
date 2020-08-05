d7680e7c3178802439aa9412ccc109f4463b4b97
RuiFeng <changruifeng98@gmail.com>
2020-08-05 18:10:03 +0800

    edge case for forms

--- src/App.js
+++ src/App.js
@@ -8,129 +8,139 @@ import { Rank } from "./components/Rank/Rank";
 import { SignIn } from "./components/SignIn/SignIn";
 import { Register } from "./components/Register/Register";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
 
 function App() {
-    const [input, setInput] = React.useState("");
-    const [image, setImage] = React.useState("");
-    const [box, setBox] = React.useState({});
-    const [route, setRoute] = React.useState("signin");
-    const [isSignedIn, setSignedIn] = React.useState(false);
-    const [user, setUser] = React.useState({
-        id: "",
-        name: "",
-        email: "",
-        entries: 0,
-        joined: "",
+  const [input, setInput] = React.useState("");
+  const [image, setImage] = React.useState("");
+  const [box, setBox] = React.useState({});
+  const [route, setRoute] = React.useState("signin");
+  const [isSignedIn, setSignedIn] = React.useState(false);
+  const [user, setUser] = React.useState({
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  });
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((response) => response.json())
+      .then(console.log);
+  }, []);
+
+  const loadUser = (data) => {
+    console.log(data);
+    setUser({
+      ...user,
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined,
     });
+  };
 
-    React.useEffect(() => {
-        fetch("http://localhost:3000/")
-            .then((response) => response.json())
-            .then(console.log);
-    }, []);
-
-    const loadUser = (data) => {
-        setUser({
-            ...user,
-            id: data.id,
-            name: data.name,
-            email: data.email,
-            entries: data.entries,
-            joined: data.joined,
-        });
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("input");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
     };
+  };
 
-    const calculateFaceLocation = (data) => {
-        const clarifaiFace =
-            data.outputs[0].data.regions[0].region_info.bounding_box;
-        const image = document.getElementById("input");
-        const width = Number(image.width);
-        const height = Number(image.height);
-        return {
-            leftCol: clarifaiFace.left_col * width,
-            topRow: clarifaiFace.top_row * height,
-            rightCol: width - clarifaiFace.right_col * width,
-            bottomRow: height - clarifaiFace.bottom_row * height,
-        };
-    };
+  const displayFaceBox = (box) => {
+    console.log(box);
+    setBox(box);
+  };
 
-    const displayFaceBox = (box) => {
-        console.log(box);
-        setBox(box);
-    };
-
-    const particlesOptions = {
-        particles: {
-            number: {
-                value: 80,
-                density: {
-                    enable: true,
-                    value_area: 800,
-                },
-            },
+  const particlesOptions = {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800,
         },
-    };
-    const app = new Clarifai.App({
-        apiKey: "0c8a5d6939344269838e5f2a28acdcc5",
-    });
-    const onInputChange = (event) => {
-        setInput(event.target.value);
-    };
-    const onSubmit = () => {
-        setImage(input);
-        app.models
-            .predict(Clarifai.FACE_DETECT_MODEL, input)
-            .then((response) => {
-                if (response) {
-                    fetch("http://localhost:3000/image", {
-                        method: "put",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            id: user.id,
-                        }),
-                    })
-                        .then((response) => response.json())
-                        .then((count) => {
-                            setUser({ ...user, entries: count });
-                        });
-                }
-                displayFaceBox(calculateFaceLocation(response));
-            })
-            .catch((err) => console.log(err));
-    };
+      },
+    },
+  };
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
 
-    const onRouteChange = (route) => {
-        if (route === "signout") {
-            setSignedIn(false);
-        } else if (route === "home") {
-            setSignedIn(true);
+  const onSignOut = () => {
+    setImage("");
+    onRouteChange("signout");
+  };
+
+  const onSubmit = () => {
+    setImage(input);
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              setUser({ ...user, entries: count });
+            });
         }
-        setRoute(route);
-    };
+        displayFaceBox(calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
+  };
 
-    return (
-        <div className="App">
-            <Particles className="particles" params={particlesOptions} />
-            <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
-            {route === "home" ? (
-                <div>
-                    <Logo />
+  const onRouteChange = (route) => {
+    if (route === "signout") {
+      setSignedIn(false);
+    } else if (route === "home") {
+      setSignedIn(true);
+    }
+    setRoute(route);
+  };
 
-                    <Rank name={user.name} entries={user.entries} />
-                    <ImageLinkForm
-                        onInputChange={onInputChange}
-                        onSubmit={onSubmit}
-                    />
-                    <FaceRecognition box={box} image={image} />
-                </div>
-            ) : route === "signin" ? (
-                <SignIn loadUser={loadUser} onRouteChange={onRouteChange} />
-            ) : (
-                <Register loadUser={loadUser} onRouteChange={onRouteChange} />
-            )}
+  return (
+    <div className="App">
+      <Particles className="particles" params={particlesOptions} />
+      <Navigation
+        onSignOut={onSignOut}
+        isSignedIn={isSignedIn}
+        onRouteChange={onRouteChange}
+      />
+      {route === "home" ? (
+        <div>
+          <Logo />
+
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
+          <FaceRecognition box={box} image={image} />
         </div>
-    );
+      ) : route === "signin" ? (
+        <SignIn loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
+        <Register loadUser={loadUser} onRouteChange={onRouteChange} />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,103 +1,94 @@
 import React from "react";
 
 export const Register = ({ onRouteChange, loadUser }) => {
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    const onNameChange = (event) => {
-        setName(event.target.value);
-    };
+  const onNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-    const onEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const onPasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const onSubmit = () => {
-        fetch("http://localhost:3000/register", {
-            method: "post",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                name: name,
-                email: email,
-                password: password,
-            }),
-        })
-            .then((response) => response.json())
-            .then((user) => {
-                if (user) {
-                    loadUser(user);
-                    onRouteChange("home");
-                }
-            });
-    };
+  const onSubmit = () => {
+    fetch("http://localhost:3000/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          loadUser(user);
+          onRouteChange("home");
+        }
+      });
+  };
 
-    return (
-        <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80">
-                <div className="measure">
-                    <fieldset
-                        id="sign_up"
-                        className="ba b--transparent ph0 mh0"
-                    >
-                        <legend className="f1 fw6 ph0 mh0">Register</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="name">
-                                Name
-                            </label>
-                            <input
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                type="email"
-                                name="name"
-                                id="name"
-                                onChange={(event) => onNameChange(event)}
-                            />
-                        </div>
-                        <div className="mt3">
-                            <label
-                                className="db fw6 lh-copy f6"
-                                htmlFor="email-address"
-                            >
-                                Email
-                            </label>
-                            <input
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                type="email"
-                                name="email-address"
-                                id="email-address"
-                                onChange={(event) => onEmailChange(event)}
-                            />
-                        </div>
-                        <div className="mv3">
-                            <label
-                                className="db fw6 lh-copy f6"
-                                htmlFor="password"
-                            >
-                                Password
-                            </label>
-                            <input
-                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                                type="password"
-                                name="password"
-                                id="password"
-                                onChange={(event) => onPasswordChange(event)}
-                            />
-                        </div>
-                    </fieldset>
-                    <div className="">
-                        <input
-                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                            type="submit"
-                            value="Register"
-                            onClick={() => onSubmit()}
-                        />
-                    </div>
-                </div>
-            </main>
-        </article>
-    );
+  return (
+    <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="name"
+                id="name"
+                onChange={(event) => onNameChange(event)}
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+                onChange={(event) => onEmailChange(event)}
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+                onChange={(event) => onPasswordChange(event)}
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Register"
+              onClick={() => onSubmit()}
+            />
+          </div>
+        </div>
+      </main>
+    </article>
+  );
 };
